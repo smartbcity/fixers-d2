7720805819c26196af934a49273aad82d46d3582
Luc Heydel <luc@smartb.city>
2021-06-22 15:10:52 +0200

    restructure interface pages

--- dokka-storybook-plugin/src/main/kotlin/d2/dokka/storybook/translators/D2StorybookPageCreator.kt
+++ dokka-storybook-plugin/src/main/kotlin/d2/dokka/storybook/translators/D2StorybookPageCreator.kt
@@ -1,14 +1,27 @@
 package d2.dokka.storybook.translators
 
+import d2.dokka.storybook.model.D2TextStyle
+import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.dokka.base.DokkaBaseConfiguration
+import org.jetbrains.dokka.base.signatures.KotlinSignatureUtils.driOrNull
 import org.jetbrains.dokka.base.signatures.SignatureProvider
 import org.jetbrains.dokka.base.transformers.pages.comments.CommentsToContentConverter
 import org.jetbrains.dokka.base.translators.documentables.DefaultPageCreator
+import org.jetbrains.dokka.base.translators.documentables.PageContentBuilder.DocumentableContentBuilder
+import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.model.DClasslike
+import org.jetbrains.dokka.model.DInterface
 import org.jetbrains.dokka.model.DModule
 import org.jetbrains.dokka.model.DPackage
+import org.jetbrains.dokka.model.DProperty
+import org.jetbrains.dokka.model.Documentable
+import org.jetbrains.dokka.model.WithScope
 import org.jetbrains.dokka.pages.ClasslikePageNode
+import org.jetbrains.dokka.pages.ContentGroup
+import org.jetbrains.dokka.pages.ContentKind
+import org.jetbrains.dokka.pages.ContentStyle
 import org.jetbrains.dokka.pages.ModulePageNode
+import org.jetbrains.dokka.pages.TextStyle
 import org.jetbrains.dokka.utilities.DokkaLogger
 
 class D2StorybookPageCreator(
@@ -25,5 +38,53 @@ class D2StorybookPageCreator(
         return ClasslikePageNode(
             c.name.orEmpty(), contentForClasslike(c), setOf(), null, emptyList()
         )
+    }
+
+    override fun contentForClasslike(c: DClasslike): ContentGroup {
+        if (c is DInterface) {
+            return contentBuilder.contentFor(c)  {
+                group(kind = ContentKind.Cover) {
+                    header(2, c.name)
+                    +contentForDescription(c)
+                }
+
+                group(styles = setOf(ContentStyle.TabbedContent)) {
+                    +contentForComments(c)
+                    +contentForScope(c, c.dri, c.sourceSets)
+                }
+            }
+        }
+
+        return super.contentForClasslike(c)
+    }
+
+    override fun contentForScope(
+        s: WithScope, dri: DRI, sourceSets: Set<DokkaConfiguration.DokkaSourceSet>
+    ): ContentGroup {
+        if (s is DInterface) {
+            return contentBuilder.contentFor(s as Documentable)  {
+                propertiesBlock(s.properties)
+            }
+        }
+
+        return super.contentForScope(s, dri, sourceSets)
+    }
+
+    private fun DocumentableContentBuilder.propertiesBlock(
+        properties: Collection<DProperty>,
+    ) {
+        block(kind = ContentKind.Properties, elements = properties) { property ->
+            text(property.name, styles = setOf(TextStyle.Italic))
+            text(property.type.driOrNull?.classNames ?: "Unknown", styles = setOf(D2TextStyle.Code))
+            group(setOf(property.dri), property.sourceSets.toSet(), ContentKind.Main) {
+                property.sourceSets.forEach { sourceSet ->
+                    property.documentation[sourceSet]?.children?.firstOrNull()?.root?.let {
+                        group(kind = ContentKind.Comment) {
+                            comment(it)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
---
+++ dokka-storybook-plugin/src/main/kotlin/d2/dokka/storybook/translators/DocumentContentBuilderExtension.kt
@@ -0,0 +1,51 @@
+package d2.dokka.storybook.translators
+
+import org.jetbrains.dokka.DokkaConfiguration
+import org.jetbrains.dokka.base.resolvers.anchors.SymbolAnchorHint
+import org.jetbrains.dokka.base.translators.documentables.PageContentBuilder
+import org.jetbrains.dokka.model.Documentable
+import org.jetbrains.dokka.model.properties.PropertyContainer
+import org.jetbrains.dokka.model.properties.plus
+import org.jetbrains.dokka.model.toDisplaySourceSets
+import org.jetbrains.dokka.pages.ContentGroup
+import org.jetbrains.dokka.pages.ContentKind
+import org.jetbrains.dokka.pages.ContentNode
+import org.jetbrains.dokka.pages.ContentTable
+import org.jetbrains.dokka.pages.DCI
+import org.jetbrains.dokka.pages.Kind
+import org.jetbrains.dokka.pages.Style
+
+fun <T: Documentable> PageContentBuilder.DocumentableContentBuilder.block(
+    kind: Kind = ContentKind.Main,
+    elements: Iterable<T>,
+    sourceSets: Set<DokkaConfiguration.DokkaSourceSet> = mainSourcesetData,
+    styles: Set<Style> = mainStyles,
+    extra: PropertyContainer<ContentNode> = mainExtra,
+    renderWhenEmpty: Boolean = false,
+    needsSorting: Boolean = true,
+    headers: List<ContentGroup> = emptyList(),
+    needsAnchors: Boolean = false,
+    operation: PageContentBuilder.DocumentableContentBuilder.(T) -> Unit
+) {
+    if (renderWhenEmpty || elements.any()) {
+        +ContentTable(
+            header = headers,
+            children = elements
+                .let {
+                    if (needsSorting)
+                        it.sortedWith(compareBy(nullsLast(String.CASE_INSENSITIVE_ORDER)) { it.name })
+                    else it
+                }
+                .map {
+                    val newExtra = if (needsAnchors) extra + SymbolAnchorHint.from(it, kind) else extra
+                    buildGroup(setOf(it.dri), it.sourceSets.toSet(), kind, styles, newExtra) {
+                        operation(it)
+                    }
+                },
+            dci = DCI(mainDRI, kind),
+            sourceSets = sourceSets.toDisplaySourceSets(),
+            style = styles,
+            extra = extra
+        )
+    }
+}
