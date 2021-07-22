a36fc2a0d6da663fe38b6a840ca4771b71da4211
Luc Heydel <luc@smartb.city>
2021-07-22 17:48:23 +0200

    new doctag @page to specify an introduction for a whole documentation page

--- dokka-storybook-plugin/src/main/kotlin/d2/dokka/storybook/model/doc/D2DocTagWrapper.kt
+++ dokka-storybook-plugin/src/main/kotlin/d2/dokka/storybook/model/doc/D2DocTagWrapper.kt
@@ -1,12 +1,18 @@
 package d2.dokka.storybook.model.doc
 
+import org.jetbrains.dokka.base.parsers.MarkdownParser
 import org.jetbrains.dokka.links.DRI
 import org.jetbrains.dokka.model.WithChildren
+import org.jetbrains.dokka.model.childrenOfType
 import org.jetbrains.dokka.model.doc.CustomTagWrapper
+import org.jetbrains.dokka.model.doc.Description
 import org.jetbrains.dokka.model.doc.DocTag
 import org.jetbrains.dokka.model.doc.DocumentationLink
+import org.jetbrains.dokka.model.doc.P
 import org.jetbrains.dokka.model.doc.Text
+import org.jetbrains.dokka.model.firstMemberOfType
 import org.jetbrains.dokka.model.firstMemberOfTypeOrNull
+import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 
 sealed class D2DocTagWrapper: WithChildren<DocTag> {
     abstract val root: DocTag
@@ -25,14 +31,50 @@ sealed class SimpleLinkTagWrapper: D2DocTagWrapper() {
 }
 
 data class D2(override val root: DocTag): SimpleTextTagWrapper()
-data class Example(override val root: DocTag): SimpleTextTagWrapper()
+data class Example(override val root: DocTag): D2DocTagWrapper() {
+    val body: String
+        get() = root.firstMemberOfType<P>()
+            .childrenOfType<Text>() // text between [] is parsed as a reference and stored in a dedicated Text tag wrapper
+            .joinToString("", transform = Text::hrefOrBody)
+}
 data class Parent(override val root: DocTag): SimpleLinkTagWrapper()
+
+data class Page(
+    override val root: DocTag
+): D2DocTagWrapper() {
+    val description: Description?
+    val example: Example?
+
+    init {
+        val childrenTags = root.children.flatMap { tag ->
+            tag.childrenOfType<Text>().flatMap { text ->
+                text.hrefOrBody()
+                    .replace("@@", "\n@")
+                    .split("\n")
+                    .filter(String::isNotBlank)
+            }
+        }.joinToString("\n")
+
+        val tags = MarkdownParser({ null }, null)
+            .parse(childrenTags)
+            .children
+            .map { child ->
+                if (child is CustomTagWrapper) child.toD2DocTagWrapper() ?: child else child
+            }
+
+        description = tags.firstIsInstanceOrNull()
+        example = tags.firstIsInstanceOrNull()
+    }
+}
 
 fun CustomTagWrapper.toD2DocTagWrapper(): D2DocTagWrapper? {
     return when (name.lowercase()) {
         "d2" -> D2(root = root)
         "example" -> Example(root = root)
         "parent" -> Parent(root = root)
+        "page" -> Page(root = root)
         else -> null
     }
 }
+
+fun Text.hrefOrBody() = params["href"] ?: body
--- sample/src/main/kotlin/sample/BoringInterface.kt
+++ sample/src/main/kotlin/sample/BoringInterface.kt
@@ -2,8 +2,20 @@ package sample
 
 /**
  * This thing is so dull that no one knows how it could have ever been created
+ *
  * @D2 model
  * @author unknown but they should be loathed for their creation
+ *
+ * @page
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+ * quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+ * Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ * @@example
+ * {
+ *   "name": "Stuff",
+ *   "description": "Phrase describing the stuff",
+ *   "things": ["Weird thingy", "Eww wat is dis"]
+ * }
  */
 interface BoringInterface {
     /**
