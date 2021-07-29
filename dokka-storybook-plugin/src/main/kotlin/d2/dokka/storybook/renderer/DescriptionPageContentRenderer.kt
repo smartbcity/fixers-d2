7d0777981d3bbe2f6e26853d4e46a6727af2b0e9
Luc Heydel <luc@smartb.city>
2021-07-29 16:41:56 +0200

    fix property rendering when empty description

--- dokka-storybook-plugin/src/main/kotlin/d2/dokka/storybook/renderer/DescriptionPageContentRenderer.kt
+++ dokka-storybook-plugin/src/main/kotlin/d2/dokka/storybook/renderer/DescriptionPageContentRenderer.kt
@@ -1,10 +1,13 @@
 package d2.dokka.storybook.renderer
 
 import d2.dokka.storybook.model.render.Article
+import org.jetbrains.dokka.model.withDescendants
 import org.jetbrains.dokka.pages.ContentGroup
 import org.jetbrains.dokka.pages.ContentPage
 import org.jetbrains.dokka.pages.ContentTable
+import org.jetbrains.dokka.pages.ContentText
 import org.jetbrains.dokka.plugability.DokkaContext
+import org.jetbrains.kotlin.utils.addToStdlib.firstIsInstanceOrNull
 
 class DescriptionPageContentRenderer(context: DokkaContext): MarkdownRenderer(context) {
 
@@ -15,7 +18,7 @@ class DescriptionPageContentRenderer(context: DokkaContext): MarkdownRenderer(co
                 append("\n")
                 property.children.forEach { child ->
                     val trailingChar = if (child is ContentGroup)
-                        if (child.children.isEmpty()) "\n\n" else ""
+                        if (child.withDescendants().firstIsInstanceOrNull<ContentText>() == null) "\n\n" else ""
                     else " "
                     append(buildString { child.build(this, pageContext) } + trailingChar)
                 }
