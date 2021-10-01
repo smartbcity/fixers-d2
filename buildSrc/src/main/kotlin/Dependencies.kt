93df184c07b9ff9b67a8141d511affd418b0646c
Luc Heydel <luc@smartb.city>
2021-10-01 11:17:10 +0200

    try using kotlint (not working)

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -6,6 +6,7 @@ object PluginVersions {
 object Versions {
 	const val junit = "5.7.0"
 	const val jackson = "2.12.3"
+	const val ktlint = "0.42.1"
 
 	const val f2 = "0.1.0-SNAPSHOT"
 }
--- dokka-storybook-plugin/build.gradle.kts
+++ dokka-storybook-plugin/build.gradle.kts
@@ -3,12 +3,22 @@ plugins {
 }
 
 dependencies {
-    compileOnly("org.jetbrains.dokka:dokka-core:${PluginVersions.dokka}")
+    compileOnly("org.jetbrains.dokka:dokka-core:${PluginVersions.dokka}"){
+//        exclude(group = "org.jetbrains.kotlin", module = "kotlin-compiler-embeddable")
+    }
     implementation("org.jetbrains.dokka:dokka-base:${PluginVersions.dokka}")
     implementation("org.jetbrains.dokka:gfm-plugin:${PluginVersions.dokka}")
     implementation(kotlin("stdlib"))
 
     implementation("com.fasterxml.jackson.core:jackson-databind:${Versions.jackson}")
+//    runtimeOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.4.32")
+//    implementation("com.pinterest.ktlint:ktlint-core:${Versions.ktlint}") {
+//        exclude(group = "org.jetbrains.kotlin", module = "kotlin-compiler-embeddable")
+//    }
+//    implementation("com.pinterest.ktlint:ktlint-ruleset-standard:${Versions.ktlint}") {
+////        exclude(group = "org.jetbrains.kotlin", module = "kotlin-compiler-embeddable")
+//    }
+
 
     testImplementation(kotlin("test-junit"))
     testImplementation("org.jetbrains.dokka:dokka-test-api:${PluginVersions.dokka}")
