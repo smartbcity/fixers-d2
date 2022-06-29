0dac60bbd427458c0b311e76bbf457c0764bac88
Luc Heydel <luc@smartb.city>
2022-06-29 12:45:46 +0200

    upgrade kotlin to 1.7.0

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,9 +1,9 @@
 
 object PluginVersions {
-	const val kotlin = "1.6.21"
-	const val dokka = "1.6.21"
+	const val kotlin = "1.7.0"
+	const val dokka = "1.7.0"
 	const val gradlePublish = "0.15.0"
-	const val fixers = "0.8.0"
+	const val fixers = "next-SNAPSHOT"
 }
 
 object Versions {
