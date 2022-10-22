d309f1d6da6cfe7554243ff0746494bd79bcf93b
Adrien Poupard <adrien@smartb.city>
2022-10-22 10:57:54 +0200

    Update kotlin version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,8 +1,8 @@
 import city.smartb.gradle.dependencies.FixersPluginVersions
 
 object PluginVersions {
-	const val kotlin = "1.7.0"
-	const val dokka = "1.7.0"
+	const val kotlin = "1.7.20"
+	const val dokka = "1.7.20"
 	const val gradlePublish = "0.15.0"
 	val fixers = FixersPluginVersions.fixers
 }
